44bad7cd7b191a939aef3678067787793f2df775
Caleb Tony <calebmbugua@gmail.com>
2021-03-28 15:36:21 +0300

    added the quote form component into the declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
